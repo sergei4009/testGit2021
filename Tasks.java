7fb305313a91a5c70d6f5dbcffd29df3bed1ed6a
sergei4009 <sergei4009@yandex.ru>
2021-10-17 13:50:04 +0300

    add file tasks.java

---
+++ Tasks.java
@@ -0,0 +1,40 @@
+package day01;
+
+import java.util.Scanner;
+
+public class Tasks
+{
+    public static void main(String[] args)
+    {
+//        int i = 0;
+//        while (i < 10)
+//        {
+//            System.out.println("Java");
+//            i++;
+//        }
+//        for (i=0;i<10;i++)
+//        {
+//            System.out.println("Java");
+//        }
+
+//        int year = 1980;
+//        while (year< 2021)
+//        {
+//            System.out.println("Олимпиада " + year + " года");
+//            year+=4;
+//        }
+//        for (int a = year ;year < 2021;year+=4)
+//        {
+//            System.out.println("Олимпиада "+year + " года");
+//        }
+
+//        int k = 3;
+//        for(int i = 1; i<10; i++)
+//        {
+//            int res = i*k;
+//            System.out.println(i +" x " + k + " = " + res);
+//        }
+
+
+    }
+}
