ffbc318016f6123a18bfe41b3e528fa121fb1df4
sergei4009 <sergei4009@yandex.ru>
2021-10-17 14:15:40 +0300

    Delete comments and change var i

--- Tasks.java
+++ Tasks.java
@@ -6,34 +6,34 @@ public class Tasks
 {
     public static void main(String[] args)
     {
-//        int i = 0;
-//        while (i < 10)
-//        {
-//            System.out.println("Java");
-//            i++;
-//        }
-//        for (i=0;i<10;i++)
-//        {
-//            System.out.println("Java");
-//        }
+       int i = 1;
+       while (i < 10)
+       {
+           System.out.println("Java");
+           i++;
+       }
+       for (i=0;i<10;i++)
+       {
+           System.out.println("Java");
+       }
 
-//        int year = 1980;
-//        while (year< 2021)
-//        {
-//            System.out.println("Олимпиада " + year + " года");
-//            year+=4;
-//        }
-//        for (int a = year ;year < 2021;year+=4)
-//        {
-//            System.out.println("Олимпиада "+year + " года");
-//        }
+       int year = 1980;
+       while (year< 2021)
+       {
+           System.out.println("Олимпиада " + year + " года");
+           year+=4;
+       }
+       for (int a = year ;year < 2021;year+=4)
+       {
+           System.out.println("Олимпиада "+year + " года");
+       }
 
-//        int k = 3;
-//        for(int i = 1; i<10; i++)
-//        {
-//            int res = i*k;
-//            System.out.println(i +" x " + k + " = " + res);
-//        }
+       int k = 3;
+       for(int i = 1; i<10; i++)
+       {
+           int res = i*k;
+           System.out.println(i +" x " + k + " = " + res);
+       }
 
 
     }
